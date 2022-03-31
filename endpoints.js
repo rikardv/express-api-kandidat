924db1fc66a3119d8d41d27b732746a8b5b3ac3a
Rikard Vestergaard <rikve590@student.liu.se>
2022-03-31 22:38:51 +0200

    Mer logisk endpoint uri

--- endpoints.js
+++ endpoints.js
@@ -1,14 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const handler = require("./handlers/handler");
+const handler = require('./handlers/handler');
 
 /**
  * Alla API endpoints defineras nedan med tillhörande handler och dess funktioner
  */
 
 /* Testing endpoints */
-router.get("/test/betyg", handler.getBetyg);
-router.get("/test/avbrott", handler.getAvbrott);
-router.get("/test/dagar", handler.getDagar);
+router.get('/test/betyg', handler.getBetyg);
+router.get('/test/avbrott', handler.getAvbrott);
+
+/*Kursavlut endpoints*/
+router.get('/kurser/avslut', handler.getDagar);
 
 module.exports = router;
