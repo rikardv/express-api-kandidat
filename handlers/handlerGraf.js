09816761e2184cd0b092690c0c2750a21ab0e56d
Max Eriksson <maxer142@student.liu.se>
2022-04-24 12:13:25 +0200

    Buggfix

--- handlers/handlerGraf.js
+++ handlers/handlerGraf.js
@@ -474,8 +474,9 @@ module.exports = {
         };
       });
 
-      result.push(...added_temp);
+      if (added_temp.length > 1) result.push(...added_temp);
     }
+    console.log(result);
     // Check if results have been returned
     let checkRes = utils.checkResultNotNull(result, res);
     if (checkRes != 0) {
