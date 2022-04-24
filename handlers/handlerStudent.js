ee444526dc3e553117ee97e015aea2c7234ab14a
Max Eriksson <maxer142@student.liu.se>
2022-04-24 18:36:18 +0200

    Hämtar tabell med betyg och antal omtentor

--- handlers/handlerStudent.js
+++ handlers/handlerStudent.js
@@ -30,12 +30,18 @@ module.exports = {
 
   getStudentGrades: async (req, res) => {
     let person_nummer = req.query.personnummer;
-    let table = await utils.sqlQuery(
-      `SELECT UTBILDNING_KOD as Kurskod, UTBILDNING_SV as Kurs, AVSER_HEL_KURS as Avklarad, BETYGSVARDE as Betyg, COUNT(CASE BETYGSVARDE WHEN 'U' then 1 else null end) as Omenta, MAX(BESLUTSDATUM) as Beslutsdatum FROM io_studieresultat WHERE PERSONNUMMER=? GROUP BY Kurskod, Kurs, Avklarad, Betyg ORDER BY Omenta DESC`,
+    let tableBetyg = await utils.sqlQuery(
+      `SELECT UTBILDNING_KOD as Kurskod, UTBILDNING_SV as Kurs, MAX(BETYGSVARDE) as Betyg, BESLUTSDATUM as Beslutsdaum FROM io_studieresultat WHERE AVSER_HEL_KURS='1' AND PERSONNUMMER= ? GROUP BY Kurskod, Kurs, Beslutsdaum`,
       person_nummer
     );
+
+    let tableOmtentor = await utils.sqlQuery(
+      `SELECT UTBILDNING_KOD as Kurskod, COUNT(CASE BETYGSVARDE WHEN 'U' then 1 else null end) as Omentor FROM io_studieresultat WHERE PERSONNUMMER=? GROUP BY Kurskod, Kurs ORDER BY Omentor DESC`,
+      person_nummer
+    );
+
     return res.status(200).send({
-      data: table,
+      data: tableBetyg,
     });
   },
 };
@@ -44,7 +50,7 @@ let createTempDB = async (person_nummer, unique_id) => {
   //Skapa en temporär databas som innehåller resultat:
   //Alla personnummer som fått ett godkänt i kurser med tillhörande start och slutdatum.
   await utils.sqlQuery(
-    `CREATE TEMPORARY TABLE TEMP_STUDENT_${unique_id} AS SELECT FORNAMN,EFTERNAMN,PERSONNUMMER,UTBILDNING_KOD,UTBILDNING_SV,YTTERSTA_KURSPAKETERING_KOD,YTTERSTA_KURSPAKETERING_SV, YTTERSTA_KURSPAKETERINGSTILLFALLE_STARTDATUM, YTTERSTA_KURSPAKETERINGSTILLFALLE_SLUTDATUM FROM IO_REGISTRERING WHERE PERSONNUMMER = ?`,
+    `CREATE TEMPORARY TABLE TEMP_STUDENT_${unique_id} AS SELECT FORNAMN,EFTERNAMN,PERSONNUMMER,UTBILDNING_KOD,UTBILDNING_SV,STUDIEPERIOD_STARTDATUM,YTTERSTA_KURSPAKETERING_KOD,YTTERSTA_KURSPAKETERING_SV, YTTERSTA_KURSPAKETERINGSTILLFALLE_STARTDATUM, YTTERSTA_KURSPAKETERINGSTILLFALLE_SLUTDATUM FROM IO_REGISTRERING WHERE PERSONNUMMER = ?`,
     person_nummer
   );
 };
